c3eb32035165dd70173d6ba124c405136892b929
unknown <purbaaditi@gmail.com>
2018-12-16 19:21:29 +0600

    11th commit

---
+++ function.php
@@ -0,0 +1,54 @@
+<?php
+
+    function showdetails($standard, $rollno)
+    {
+        include('dbcon.php');
+        $sql = "SELECT * FROM `student` WHERE rollno='$rollno' AND standard='$standard'";
+        $run = mysqli_query($con, $sql);
+        
+        if(mysqli_num_rows($run) > 0)
+        {
+            $data = mysqli_fetch_assoc($run);
+?>
+            <table border="1" style="width:50%; margin-top: 20px;" align="center" >
+                <tr>
+                    <th colspan="3">Student Details</th>
+                </tr>
+                <tr>
+                    <td rowspan="5"><img src="dataimg/<?php echo $data['image']; ?>" style="max-width: 120px; max-height: 150px;" /></td>
+                    <th>Roll No</th>
+                    <td><?php echo $data['rollno'] ?></td>
+                </tr>
+                <tr>
+                    <th>Name</th>
+                    <td><?php echo $data['name'] ?></td>
+                </tr>
+                <tr>
+                    <th>Standard</th>
+                    <td><?php echo $data['standard'] ?></td>
+                </tr>
+                <tr>
+                    <th>Parents Contact No</th>
+                    <td><?php echo $data['pcont'] ?></td>
+                </tr>
+                <tr>
+                    <th>City</th>
+                    <td><?php echo $data['city'] ?></td>
+                </tr>
+            </table>
+<?php
+
+        }
+        else 
+        {
+            ?>
+        }
+            <script>
+            alert ('No Student Found !!! ');
+            window.open('index.php','_self');
+            </script>
+            <?php
+             
+        }  
+    }
+?>
